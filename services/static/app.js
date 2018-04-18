ce3467cb9a0f58550bc051e6d92b175f00b9531d
Anton Gilgur <agilgur5@gmail.com>
2018-04-18 02:11:23 -0700

    (feat): add initial Kitty Viewer interface
    - Task 1 (part 2)
    
    - create a simple search app in React that should render the token list
      - leave it all in one file for now as it's small
    
    - run into a bug with `tokensOfOwner` always returning an empty array
      - it doesn't throw an error though...
      - apparently it's timing out on the extremely inefficient
        Solidity code :/ (so the interwebz AKA reddit says)
        - EtherScan always gets an empty array too

--- services/static/app.js
+++ services/static/app.js
@@ -1,3 +1,6 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
 import Web3 from 'web3'
 
 import { address, abi } from './contract.js'
@@ -14,3 +17,48 @@ if (typeof window.web3 != 'undefined'){
 }
 const contract = web3.eth.contract(abi)
 const contractInst = contract.at(address)
+
+class App extends Component {
+  state = {
+    userAddress: '',
+    loading: false,
+    tokens: []
+  }
+  handleChange = (ev) => {
+    let value = ev.target.value // save event before it changes
+    this.setState({userAddress: value, loading: true})
+    contractInst.tokensOfOwner(value, (err, result) => {
+      if (err) {
+        console.error('tokensOfOwner call failed!')
+        throw new Error(err)
+      }
+      console.log(result)
+      this.setState({tokens: result, loading: false})
+    })
+  }
+  render = () => {
+    let {userAddress, loading, tokens} = this.state
+    return <div>
+      Search by User Address:
+      <br />
+      <input type='text' value={userAddress} onChange={this.handleChange} />
+      <br />
+      {tokens.length > 0
+        ? <div>
+          Showing tokens for this address:
+          <ul>
+            {tokens.map((token) => {
+              <li>{token}</li>
+            })}
+          </ul>
+        </div>
+        : loading
+          ? 'Loading...'
+          : 'No tokens available for this address.'}
+    </div>
+  }
+}
+
+const element = document.createElement('div')
+document.body.appendChild(element)
+ReactDOM.render(<App />, element)
