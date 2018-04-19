70a8fcc5c7890a8639486f5623230a7a2639a903
Anton Gilgur <agilgur5@gmail.com>
2018-04-18 23:20:16 -0700

    (feat/fix): functioning Kitty Viewer using CK API
    - Task 1 part 2 "fix"
    
    - remove the not functional tokensOfOwner contract method,
      replace with CryptoKitties API call
    
    - show tokens as just list of JSON for now (no requirement
      on how it should look)
      - so BRUTALIST
      - maybe I should use Preact if I wanted to go hardcore on killing bloat
    - fix up syntax error in map function and add keys as well
    
    - show the error to the user in text if one occurs

--- services/static/app.js
+++ services/static/app.js
@@ -18,26 +18,40 @@ if (typeof window.web3 != 'undefined'){
 const contract = web3.eth.contract(abi)
 const contractInst = contract.at(address)
 
+/* get Kitties from the API because tokensOfOwner contract method is too slow
+   to ever work */
+function getKitties (address) {
+  // note that this defaults to only displaying the first 12 kitties
+  // for this purpose I think that's okay, but one could easily implement
+  // pagination or infinite scroll if one so wanted to
+  return fetch(
+    'https://api.cryptokitties.co/kitties?owner_wallet_address=' + address
+  ).then((resp) => resp.json())
+}
+
 class App extends Component {
   state = {
     userAddress: '',
     loading: false,
+    error: false,
     tokens: []
   }
   handleChange = (ev) => {
     let value = ev.target.value // save event before it changes
-    this.setState({userAddress: value, loading: true})
-    contractInst.tokensOfOwner(value, (err, result) => {
-      if (err) {
-        console.error('tokensOfOwner call failed!')
-        throw new Error(err)
-      }
-      console.log(result)
-      this.setState({tokens: result, loading: false})
-    })
+    this.setState({userAddress: value, loading: true, error: false})
+    getKitties(value)
+      .catch((err) => {
+        console.error('CryptoKitties API call failed!')
+        alert('CryptoKitties API call failed!') // leave error handling to user
+        this.setState({loading: false, error: true})
+      })
+      .then((result) => {
+        console.log(result)
+        this.setState({tokens: result.kitties, loading: false})
+      })
   }
   render = () => {
-    let {userAddress, loading, tokens} = this.state
+    let {userAddress, loading, error, tokens} = this.state
     return <div>
       Search by User Address:
       <br />
@@ -47,14 +61,16 @@ class App extends Component {
         ? <div>
           Showing tokens for this address:
           <ul>
-            {tokens.map((token) => {
-              <li>{token}</li>
-            })}
+            {tokens.map((token) =>
+              <li key={token.id}>{JSON.stringify(token)}</li>
+            )}
           </ul>
         </div>
         : loading
           ? 'Loading...'
-          : 'No tokens available for this address.'}
+          : error
+            ? 'CryptoKitties API call failed! Maybe try typing again?'
+            : 'No tokens available for this address.'}
     </div>
   }
 }
